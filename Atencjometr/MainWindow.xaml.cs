62bdc8f2c0fbe1df7fdb38b42917711656907645
malinowskae <malinowskae@DESKTOP41>
2016-09-05 08:52:51 +0200

    poprawki

--- Atencjometr/MainWindow.xaml.cs
+++ Atencjometr/MainWindow.xaml.cs
@@ -30,17 +30,26 @@ namespace Atencjometr
         private void btn_compare_Click(object sender, RoutedEventArgs e)
         {
 
-            tagim1.Selection.Text = Wypok.ConnectToMirko.WypiszTagi(mirek1.Text);
-            tagim2.Selection.Text = Wypok.ConnectToMirko.WypiszTagi(mirek2.Text);
-
+            try
+            {
+                tagim1.Selection.Text = Wypok.ConnectToMirko.WypiszTagi(mirek1.Text);
+                tagim2.Selection.Text = Wypok.ConnectToMirko.WypiszTagi(mirek2.Text);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Error{0}", exc.ToString());
+                throw;
+            }
             string link1 = "http://bezpiecznykot.pl/wp-content/uploads/2015/12/kot-150x150.jpg";
-        
-            avm1.Source = new ImageSourceConverter().ConvertFromString(link1) as ImageSource;
-          
-            //string link = Wypok.ConnectToMirko.PobierzAvatar(mirek1.Text);
-            //avm1.Source = new BitmapImage(new Uri(link));
-            // avm2.Source = new BitmapImage(new Uri(Wypok.ConnectToMirko.PobierzAvatar(mirek2.Text)));
-            lbl_wspolne.Content = Wypok.ConnectToMirko.CommonTags(mirek1.Text, mirek2.Text);
+
+                avm1.Source = new ImageSourceConverter().ConvertFromString(link1) as ImageSource;
+
+                //string link = Wypok.ConnectToMirko.PobierzAvatar(mirek1.Text);
+                //avm1.Source = new BitmapImage(new Uri(link));
+                // avm2.Source = new BitmapImage(new Uri(Wypok.ConnectToMirko.PobierzAvatar(mirek2.Text)));
+                lbl_wspolne.Content = Wypok.ConnectToMirko.CommonTags(mirek1.Text, mirek2.Text);
+            
+           
 
 
 
--- AtjWinForms/Form1.cs
+++ AtjWinForms/Form1.cs
@@ -24,7 +24,33 @@ namespace AtjWinForms
 
             var nick1 = txtbNick1.Text;
             var nick2 = txtBNick2.Text;
-            
+            if (nick1.Length==0 ||nick2.Length==0)
+            {
+                MessageBox.Show("Wpisz oba nicki");
+                return;
+            }
+            try
+            {
+                Wypok.GetResponses.GetResponseFromWypok(nick1);
+                Wypok.GetResponses.GetResponseFromWypok(nick2);
+            }
+            catch (WebException ex) when ((ex.Response as HttpWebResponse)?.StatusCode == HttpStatusCode.NotFound)
+            {
+                MessageBox.Show("Nie istnieje taki mirek");
+                return;
+            }
+            catch (WebException ex) when ((ex.Response as HttpWebResponse)?.StatusCode == HttpStatusCode.ServiceUnavailable)
+            {
+                MessageBox.Show("Serwer nie odpowiada");
+                return;
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Cos sie... cos sie popsuło");
+                return;
+            }
+
+
             rtxtBTagi1.Text = ConnectToMirko.WypiszTagi(txtbNick1.Text);
             rtxtBTagi2.Text = ConnectToMirko.WypiszTagi(txtBNick2.Text);
             avMirek1.Image = ConnectToMirko.UstawAvek(txtbNick1.Text);
